57a72a6a576223a90b882d6536303c4aa1813ba7
Vyom Jani <vyomjani2@gmail.com>
2026-09-25 21:31:23 +0530

    feat: stream file contents to stdout instead of reading the whole file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     env, fs,
-    io::{self, BufReader, Read, Write},
+    io::{self, BufReader},
     process, result,
 };
 
@@ -22,16 +22,8 @@ fn main() {
         }
     }
 
-    match read_file_contents(&file_path) {
-        Ok(fc) => {
-            let stdout = io::stdout();
-            let mut handle = stdout.lock();
-
-            if let Err(error) = handle.write_all(&fc) {
-                eprintln!("ERROR: couldn't write file contents to stdout. {}", error);
-                process::exit(1)
-            }
-        }
+    match display_file_contents(&file_path) {
+        Ok(_) => {}
         Err(error) => {
             eprintln!("{error}");
             process::exit(1);
@@ -39,14 +31,16 @@ fn main() {
     }
 }
 
-fn read_file_contents(file_path: &String) -> result::Result<Vec<u8>, io::Error> {
+fn display_file_contents(file_path: &str) -> result::Result<(), io::Error> {
     let file_object = fs::File::open(file_path)?;
+    let stdout = io::stdout();
 
     let mut buffer_reader = BufReader::new(file_object);
-    let mut contents: Vec<u8> = Vec::new();
-    buffer_reader.read_to_end(&mut contents)?;
+    let mut handle = stdout.lock();
 
-    Ok(contents)
+    io::copy(&mut buffer_reader, &mut handle)?;
+
+    Ok(())
 }
 
 fn validate_filepath(file_path: &str) -> result::Result<(), io::Error> {
